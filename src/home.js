3ad1ac6939ec5ae9b51480a1ef1a98f2be0cf02e
abdusali1234 <abdusali1234@gmail.com>
2024-07-27 10:49:34 +0100

    Added opening hours info to home page. Added mode to webpack.config.js

--- src/home.js
+++ src/home.js
@@ -9,14 +9,67 @@ export default function loadHome () {
     content.appendChild(chaiPic);
 
     const title = document.createElement('h1');
-    title.innerHTML = "Affa's Saa";
+    title.innerText = "Affa's Saa";
     content.appendChild(title);
 
-    const about = document.createElement('p');
+    const about = document.createElement('section');
+    about.setAttribute("id", "about");
     about.innerHTML = `
-            Welcome to Affa's Saa, the best Chai cafe in the UK!
+            <p>Welcome to Affa's Saa, the best Chai cafe in the UK!
             We source our organic tea leaves from the lush tree gardens of Sylhet.
             We support the women who pick our premium tea leaves, learn more <a href="#">here</a>.
-            `;
+            </p>`;
     content.appendChild(about);
+
+    const openHours = document.createElement('section');
+    openHours.setAttribute("id", "opening-hours");
+    const hoursHeading = document.createElement('h2');
+    hoursHeading.innerText = "Hours";
+    openHours.appendChild(hoursHeading);
+    const hoursTable = document.createElement("table");
+    hoursTable.setAttribute("id", "hours-table");
+    hoursTable.innerHTML = `
+            <tr>
+                <th>Day</th>
+                <th>Opening Time</th>
+                <th>Closing Time</th>
+            </tr>
+            <tr>
+                <td>Monday</td>
+                <td>8am</td>
+                <td>6pm</td>
+            </tr>
+            <tr>
+                <td>Tuesday</td>
+                <td>8am</td>
+                <td>6pm</td>
+            </tr>
+            <tr>
+                <td>Wednesday</td>
+                <td>8am</td>
+                <td>6pm</td>
+            </tr>
+            <tr>
+                <td>Thursday</td>
+                <td>8am</td>
+                <td>6pm</td>
+            </tr>
+            <tr>
+                <td>Friday</td>
+                <td>8am</td>
+                <td>6pm</td>
+            </tr>
+            <tr>
+                <td>Saturday</td>
+                <td>8am</td>
+                <td>6pm</td>
+            </tr>
+            <tr>
+                <td>Sunday</td>
+                <td>10am</td>
+                <td>4pm</td>
+            </tr>
+            `
+    openHours.appendChild(hoursTable);
+    content.appendChild(openHours);
 }
