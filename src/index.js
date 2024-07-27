15b8dc01caeabe11d4aa0e9cc0af3808ea402047
abdusali1234 <abdusali1234@gmail.com>
2024-07-27 11:40:40 +0100

    Created menu page. Added functionality for home and menu buttons to change page content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import './style.css';
 import loadHome from './home';
+import loadMenu from './menu';
+
+const homeBtn = document.getElementById("home");
+const menuBtn = document.getElementById("menu");
+const contactBtn = document.getElementById("contact");
+
+
 
 loadHome();
 
+homeBtn.addEventListener('click', () => {
+    console.log("clicking home");
+    loadHome();
+});
+menuBtn.addEventListener('click', () => {
+    console.log("clicking menu");
+    loadMenu();
+});
 console.log("Test!!!")
---
+++ src/menu.js
@@ -0,0 +1,49 @@
+
+
+export default function loadMenu () {
+    const content = document.getElementById('content');
+    content.innerHTML = "";
+
+
+    const title = document.createElement('h1');
+    title.innerText = "Menu";
+    content.appendChild(title);
+
+    const beverages = document.createElement('section');
+    beverages.setAttribute("id", "beverages");
+    const beveragesHeading = document.createElement("h2");
+    beveragesHeading.innerText = "Beverages";
+    beverages.appendChild(beveragesHeading);
+
+    const drinks =[
+        {name: "Black Tea", price: "£1.75"},
+        {name: "Karack Chai", price: "£2.50"},
+        {name: "Gur Chai", price: "£3.25"},
+        {name: "Masala Chai", price: "£3.25"},
+        {name: "Turmeric Latte", price: "£3.50"},
+        {name: "Double Shot Espresso Nespresso Macchiato Molto Bene Supreme", price: "£5"}
+    ];
+
+    const drinksTable = document.createElement("table");
+    drinksTable.setAttribute("id", "drinks-table");
+    drinksTable.innerHTML = `
+            <tr>
+                <th>Drink</th>
+                <th>Price</th>
+            </tr>
+            `;
+    drinks.forEach((drink) => {
+        const drinksRow = `
+                <tr>
+                    <td>${drink.name}</td>
+                    <td>${drink.price}</td>
+                </tr>
+                `;
+        drinksTable.innerHTML += drinksRow;
+    });
+    beverages.appendChild(drinksTable);
+    content.appendChild(beverages);
+
+
+    
+}
